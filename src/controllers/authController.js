1ccdecf763ddc3109e7eba24a74a03803f0fce42
Daniel Kamenov <sorostmcoc@gmail.com>
2023-06-05 19:36:16 +0300

    add auth pages

---
+++ src/controllers/authController.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+router.post('/login', (req, res) => {
+    const {email, password} = req.body;
+
+
+});
+
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+router.post('/register', (req, res) => {
+    const {username, email, password, rePassword} = req.body;
+
+    
+});
+
+module.exports = router;
